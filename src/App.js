d4292799d6ac0609edecd099aa139f597a5bef6b
Your Actual Name Here <SHA256:hOXSm53Eq3p5Z4sDLk3h/LjyDO3EhgoJf0cCmjpAP04>
2022-07-13 23:16:33 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import Validator from "./components/Validator"
+import { useState } from 'react'
+import Validator from "./components/Validator.jsx"
 
 const App = () => {
+  const [username, setUsername] = useState('')
+  const handleChange = (event) => {
+    event.preventDefault()
+    setUsername(event.target.value)
+   }
+   const [password, setPassword] = useState('')
+   const handleChange1= (event) => {
+    event.preventDefault()
+    setPassword(event.target.value)
+   }
+   const [passwordConfirm, setPasswordConfirm] = useState('')
+   const handleChange2= (event) => {
+    event.preventDefault()
+    setPasswordConfirm(event.target.value)
+   }
+   let [valid, setValid] = useState('')
+   const handleChange3= (event) => {
+    event.preventDefault()
+    setValid(event.target.value)
+   }
 
+
+const validPassword = () => {
+  if (password === passwordConfirm) {
+    valid = true
+    alert('Welcome!')
+  } else {
+   valid = false
+   alert('Password does not match!')
+  }
+}
+  
   return (
-    <Validator />
+    <Validator handleChange={handleChange} handleChange1={handleChange1} handleChange2={handleChange2} handleChange3={handleChange3} username={username} password={password}passwordConfirm={passwordConfirm} valid={valid} validPassword={validPassword}/>
   )
 }
 
--- src/components/Validator.jsx
+++ src/components/Validator.jsx
@@ -1,25 +1,26 @@
 import '../styles/Validator.css'
 
-const Validator = () => {
+const Validator = (props) => {
   
   return (
     <div className="form">
       <h1>Sign Up</h1>
       <form>
-        <input type="text" placeholder="Username" id="username" />
+        <input type="text" placeholder="Username" id="username"
+        onChange={props.handleChange} value={props.username}/>
         <label htmlFor="username">Username</label>
 
-        <input type="password" placeholder="Password" id="password" />
+        <input type="password" placeholder="Password" id="password" onChange={props.handleChange1} value={props.password}/>
         <label htmlFor="password">Password</label>
 
         <input
           type="password"
           placeholder="Confirm password"
           id="passwordConfirm"
-        />
+          onChange={props.handleChange2} value={props.confirmPassword}/>
         <label htmlFor="passwordConfirm">Confirm password</label>
-
-        <button type="submit">Sign Up</button>
+        
+        <button type="submit" onClick={props.validPassword} onChange={props.handleChange3}value={props.valid}>Sign Up</button>
         <p>Passwords must match.</p>
       </form>
     </div>
